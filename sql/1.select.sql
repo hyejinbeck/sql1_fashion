1f4cc4270065f2ca661d8465af8b00e3928eb0b2
hyejin beck <beckhye@gmail.com>
2023-09-04 23:49:42 +0900

    SELECT 기본

--- sql/1.select.sql
+++ sql/1.select.sql
@@ -1,1 +1,17 @@
 SELECT * FROM fashions_user;
+-- 총 50명의 가상 User 정보가 나온다 (name,country,,) 
+
+SELECT DISTINCT name FROM fashions_category; 
+-- 일단 skirts 하나만 나온다 (왜지? 많은 카테고리를 지정했는데...) 
+
+SELECT * FROM fashions_design;
+-- 4가지 값만 나온다 (color, customer_age, mood) 무작위 
+
+SELECT * FROM fashions_item; 
+-- 데이터값 없음 
+
+SELECT * FROM fashions_item_categories; 
+-- 데이터값 없음 
+
+SELECT * FROM fashions_payment; 
+-- 데이터값 없음 
